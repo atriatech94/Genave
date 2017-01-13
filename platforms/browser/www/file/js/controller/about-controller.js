8b2317b0cf23c6c27cfb7e5c37ba4d67b8b524f0
atriatech94 <hamid_7555@hotmail.com>
2017-01-13 21:57:00 +0330

    n333

--- platforms/browser/www/file/js/controller/about-controller.js
+++ platforms/browser/www/file/js/controller/about-controller.js
@@ -1,4 +1,28 @@
- app.controller('aboutController', function($scope) { 
-          $scope.map = "http://maps.googleapis.com/maps/api/staticmap?key=AIzaSyAgsQ1ds-j3GBd4Yz07dHlMiiD33h1r1Jk&center=29.5838591,50.5062979&zoom=16&scale=false&size=600x300&maptype=roadmap&format=jpg&visual_refresh=true&markers=size:mid%7Ccolor:0xf40b6f%7Clabel:%7C29.5838591,50.5062979" ;
-
- })
+ app.controller('aboutController', function($scope,$http) { 
+      $scope.base_img = uploads_pic;
+      if(localStorage.getItem('about_us') != null){
+         $scope.about_us = JSON.parse(localStorage.getItem('about_us'));
+      }
+      else
+      {
+        document.getElementById('loading').removeAttribute('style'); 
+      }
+      
+      $http({
+            method: 'GET',
+            url: base_url+'about_us', 
+            headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+        }).then(function successCallback(response) {
+               document.getElementById('loading').setAttribute('style','display:none;'); 
+               $scope.about_us = response.data.about; 
+               console.log($scope.about_us);
+               localStorage.setItem('about_us',JSON.stringify($scope.about_us)); 
+         }, function errorCallback(response) {
+                    document.getElementById('loading').setAttribute('style','display:none;'); 
+                    ons.notification.alert({
+                    title: 'خطا',
+                    buttonLabel:"بستن " ,
+                    message: 'خطا در برقراری ارتباط با سرور'
+                }); 
+       }); 
+ });
